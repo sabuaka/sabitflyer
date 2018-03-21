bbc66c78d4965262c896b4790d982b52e2eaa07a
sabuaka <sabuaka-fx@hotmail.com>
2018-03-21 16:02:15 +0900

    [mod] 現時点のバージョンを設定

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='sabitflyer',
     packages=['sabitflyer'],
-    version='0.0.1',
+    version='0.0.2',
     description='Bitflyer API Library for Python',
     author='sabuaka',
     author_email='sabuaka-fx@hotmail.com',
