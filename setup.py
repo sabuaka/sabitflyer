a5579a023b012eb0c51c4f56738a5da1423322fd
sabuaka <sabuaka-fx@hotmail.com>
2018-10-01 11:45:59 +0900

    [mod](ver2.7.2) Specify version of install_requires.

--- setup.py
+++ setup.py
@@ -3,10 +3,14 @@ from setuptools import setup
 setup(
     name='sabitflyer',
     packages=['sabitflyer'],
-    version='2.7.1',
+    version='2.7.2',
     description='bitFlyer API Library for Python',
     author='sabuaka',
     author_email='sabuaka-fx@hotmail.com',
     url="https://github.com/sabuaka/sabitflyer",
-    install_requires=['requests', 'urllib3', 'websocket-client']
+    install_requires=[
+        'requests==2.19.1',
+        'urllib3==1.23',
+        'websocket-client==0.48.0'
+    ]
 )
