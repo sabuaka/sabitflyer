9b36391d1719fbe6fc699cc07d7d359786f6722d
sabuaka <sabuaka-fx@hotmail.com>
2019-01-16 08:57:54 +0900

    [mod](ver3.0.1) Removed security vulnerability in requests module.

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     name='sabitflyer',
     packages=['sabitflyer'],
-    version='3.0.0',
+    version='3.0.1',
     description='bitFlyer API Library for Python',
     author='sabuaka',
     author_email='sabuaka-fx@hotmail.com',
     url="https://github.com/sabuaka/sabitflyer",
     install_requires=[
-        'requests==2.19.1',
+        'requests==2.21.0',
         'urllib3==1.23',
         'websocket-client==0.48.0'
     ]
