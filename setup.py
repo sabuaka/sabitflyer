47fce2470cbb741dfd62be1ee115c7067ec9cbb4
sabuaka <sabuaka-fx@hotmail.com>
2019-05-21 08:12:09 +0900

    [mod](ver3.2.3) Update due to security alert.

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 setup(
     name='sabitflyer',
     packages=['sabitflyer'],
-    version='3.2.2',
+    version='3.2.3',
     description='bitFlyer API Library for Python',
     author='sabuaka',
     author_email='sabuaka-fx@hotmail.com',
     url="https://github.com/sabuaka/sabitflyer",
     install_requires=[
         'requests==2.21.0',
-        'urllib3==1.25.2',
+        'urllib3==1.24.3',
         'websocket-client==0.48.0'
     ]
 )
